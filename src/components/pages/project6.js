d3d5926be911987eab29e7350afe5bf6c83f3ae0
lzj-0 <97489546+lzj-0@users.noreply.github.com>
2022-10-31 18:29:23 +0800

    Update project6.js

--- src/components/pages/project6.js
+++ src/components/pages/project6.js
@@ -5,6 +5,7 @@ import React, { useState, useEffect } from "react";
 import "@fontsource/sora"
 import { ethers } from "ethers";
 import { parseBytes32String, parseEther } from "ethers/lib/utils";
+import { abi } from "abi";
 // import Image from "next/image";
 let provider = new ethers.providers.Web3Provider(window.ethereum);
 let signer = provider.getSigner();
